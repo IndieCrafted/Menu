03d4a0e239c722f91866c5369a4f50a8d99434bb
KidneyFlower <753475777@qq.com>
2019-04-08 23:47:10 +0800

    新增 Menu 页面

--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Menu from './views/Menu'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: '/menu/:page',
+      name: 'menu',
+      component: Menu
+    }
+  ]
 })
---
+++ src/views/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="menu">
+    Menu
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      page: this.$route.params.page
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.menu {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
