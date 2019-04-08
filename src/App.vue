5996c26714103be9cd5db06557208fe5111345e9
KidneyFlower <753475777@qq.com>
2019-04-08 23:45:51 +0800

    修改根组件及其样式

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  created () {
+    this.calcFontSize()
+  },
+  methods: {
+    calcFontSize () {
+      const docElement = document.documentElement
+      const initialFontSize = 100 * (docElement.clientWidth / 1920)
+      const fontSize = initialFontSize >= 100 ? 100 : initialFontSize
+      docElement.style.fontSize = `${fontSize}px`
     }
   }
 }
+</script>
+
+<style lang="scss">
+@import '@/assets/_base.scss';
 </style>
