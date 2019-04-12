16025d3d3bf8c3d289a9188b0a9c8cc2718192bf
KidneyFlower <753475777@qq.com>
2019-04-12 15:11:04 +0800

    完善 MenuItem 组件

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="menu-item">
     <div class="left">
-      <span class="no">4</span>
+      <span class="number">{{ data.number }}</span>
       <div>
-        <p>
-          <span class="company">气泡实验室</span>
-          <span class="name">及时雨浑浊IPA</span>
-          <span class="addr">嘉善</span>
+        <p class="basic">
+          <span class="brand">{{ data.brand }}</span>
+          <span class="name">{{ data.name }}</span>
+          <span class="origin">{{ data.origin }}</span>
         </p>
         <p class="en-name">
-          Bubble Lab Juicy Rain New England IPA
+          {{ data.enName }}
         </p>
       </div>
     </div>
     <div class="center">
       <div class="info">
-        <p>风格：浑浊IPA</p>
-        <p>酒精度：7.2%</p>
+        <p>风格：{{ data.style }}</p>
+        <p>酒精度：{{ data.vol }}</p>
       </div>
     </div>
     <div class="right">
       <div class="sku">
-        <span class="price">40元</span>
-        <span class="nwt">400ml</span>
+        <span class="price">{{ ~~data.price }}元</span>
+        <span class="spec">{{ data.spec }}ml</span>
       </div>
     </div>
   </div>
@@ -30,7 +30,12 @@
 
 <script>
 export default {
-
+  props: {
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  }
 }
 </script>
 
@@ -41,58 +46,68 @@ export default {
   flex: 1;
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
-  padding: 0 0.5rem;
+  padding: 0 0.4rem;
   &:nth-child(2n) {
     background: #26303d;
   }
   .left {
+    min-width: 60%;
     font-size: 0;
     display: flex;
     align-items: center;
-    .no {
+    .number {
       color: #ddd;
-      font-size: 0.3rem;
-      margin-right: 0.5rem;
+      font-size: 0.4rem;
+      margin-right: .4rem;
     }
-    .company {
-      font-size: 0.5rem;
-      color: #ddd;
-      margin-right: 0.1rem;
-    }
-    .name {
-      font-size: 0.5rem;
-      font-weight: bold;
-      margin-right: 0.1rem;
+    .basic {
+      line-height: 1;
+      .brand {
+        font-size: 0.7rem;
+        color: #ddd;
+        margin-right: 0.2rem;
+      }
+      .name {
+        font-size: 0.7rem;
+        margin-right: 0.2rem;
+        font-family: 'CuHei';
+      }
+      .origin {
+        font-size: 0.35rem;
+        color: #ddd;
+      }
     }
     .en-name {
-      font-size: 0.3rem;
-    }
-    .addr {
-      font-size: 0.25rem;
-      color: #ddd;
+      line-height: 1;
+      margin-top: .1rem;
+      font-size: 0.35rem;
+      font-family: 'En';
     }
   }
   .center {
+    flex: 1;
     .info {
       font-size: 0;
       p {
-        font-size: 0.25rem;
+        font-size: 0.35rem;
         color: #ddd;
       }
     }
   }
   .right {
+    flex: 1;
+    text-align: right;
     .sku {
       font-size: 0;
       .price {
-        font-size: 0.5rem;
-        font-weight: bold;
-        margin-right: 0.1rem;
+        font-size: 0.7rem;
+        margin-right: 0.2rem;
+        font-family: 'CuHei';
       }
-      .nwt {
-        font-size: 0.25rem;
+      .spec {
+        font-size: 0.35rem;
         color: #ddd;
       }
     }
